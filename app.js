60b3b4e88583a84877a3993c91001284f158a2ff
Pedro Alves <3pedru@gmail.com>
2017-05-17 22:40:22 -0300

    few adjusts

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/:abc?', function(req, res) {
 })
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on http://localhost:3000');
 })
