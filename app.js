6729974fc2c748017c52a8313a940dfb2c6b6477
Pedro Alves <3pedru@gmail.com>
2017-05-17 22:27:10 -0300

    simple rustic poc with express js and query string param

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const http = require('http'),
-port = 3000,
-express = require('express'),
-app = express(),
-bodyParser = require('body-parser'),
-helmet = require('helmet')
+'use strict';
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const http = require('http'),
+  port = 3000,
+  express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  helmet = require('helmet'),
+  session = require('express-session')
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 app.use(helmet())
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(session({
+  secret: 'abc-123-def-987',
+  resave: false,
+  saveUninitialized: false
+}))
 
-app.listen(3000, function () {
+app.get('/:abc?', function(req, res) {
+  let abcList = []
+  if (req.query.abc) {
+    if (!!req.session.abcList) {
+      abcList = req.session.abcList
+    }
+    abcList.push(req.query.abc)
+    req.session.abcList = abcList
+  }
+  res.send('<h1>Please send a query string param named abc</h1><h2>here the abc list in your session:</h2>' + req.session.abcList);
+})
+
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
-});
+})
